918e32a773844346056fc87ed9cf00d21af3cd74
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-09-25 22:48:50 +0300

    compilation

--- src/main/java/org/jetbrains/plugins/javaFX/fxml/codeInsight/JavaFxImportsOptimizer.java
+++ src/main/java/org/jetbrains/plugins/javaFX/fxml/codeInsight/JavaFxImportsOptimizer.java
@@ -19,7 +19,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Nonnull;
@@ -44,8 +46,7 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.PsiMember;
 import com.intellij.psi.XmlRecursiveElementVisitor;
-import com.intellij.psi.codeStyle.CodeStyleSettings;
-import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
+import com.intellij.psi.codeStyle.JavaCodeStyleSettings;
 import com.intellij.psi.impl.source.codeStyle.ImportHelper;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttribute;
@@ -63,124 +64,166 @@ import com.intellij.xml.XmlElementDescriptor;
  * User: anna
  * Date: 2/22/13
  */
-public class JavaFxImportsOptimizer implements ImportOptimizer {
-  @Override
-  public boolean supports(PsiFile file) {
-    return JavaFxFileTypeFactory.isFxml(file);
-  }
+public class JavaFxImportsOptimizer implements ImportOptimizer
+{
+	@Override
+	public boolean supports(PsiFile file)
+	{
+		return JavaFxFileTypeFactory.isFxml(file);
+	}
 
-  @Nonnull
-  @Override
-  public Runnable processFile(final PsiFile file) {
-    VirtualFile vFile = file.getVirtualFile();
-    if (vFile instanceof VirtualFileWindow) vFile = ((VirtualFileWindow)vFile).getDelegate();
-    final Project project = file.getProject();
-    if (vFile == null || !ProjectRootManager.getInstance(project).getFileIndex().isInSourceContent(vFile)) {
-      return EmptyRunnable.INSTANCE;
-    }
-    final List<Pair<String, Boolean>> names = new ArrayList<Pair<String, Boolean>>();
-    collectNamesToImport(names, (XmlFile)file);
-    Collections.sort(names, new Comparator<Pair<String, Boolean>>() {
-      @Override
-      public int compare(Pair<String, Boolean> o1, Pair<String, Boolean> o2) {
-        return StringUtil.compare(o1.first, o2.first, true);
-      }
-    });
-    final CodeStyleSettings settings = CodeStyleSettingsManager.getSettings(project);
-    final List<Pair<String, Boolean>> sortedNames = ImportHelper.sortItemsAccordingToSettings(names, settings);
-    final HashSet<String> onDemand = new HashSet<String>();
-    ImportHelper.collectOnDemandImports(sortedNames, onDemand, settings);
-    final Set<String> imported = new HashSet<String>();
-    final List<String> imports = new ArrayList<String>();
-    for (Pair<String, Boolean> pair : sortedNames) {
-      final String qName = pair.first;
-      final String packageName = StringUtil.getPackageName(qName);
-      if (imported.contains(packageName) || imported.contains(qName)) {
-        continue;
-      }
-      if (onDemand.contains(packageName)) {
-        imported.add(packageName);
-        imports.add("<?import " + packageName + ".*?>");
-      } else {
-        imported.add(qName);
-        imports.add("<?import " + qName + "?>");
-      }
-    }
-    final PsiFileFactory factory = PsiFileFactory.getInstance(file.getProject());
-    
-    final XmlFile dummyFile = (XmlFile)factory.createFileFromText("_Dummy_.fxml", XmlFileType.INSTANCE, StringUtil.join(imports, "\n"));
-    final XmlDocument document = dummyFile.getDocument();
-    final XmlProlog newImportList = document.getProlog();
-    if (newImportList == null) return EmptyRunnable.getInstance();
-    return new Runnable() {
-      @Override
-      public void run() {
-        final XmlDocument xmlDocument = ((XmlFile)file).getDocument();
-        final XmlProlog prolog = xmlDocument.getProlog();
-        if (prolog != null) {
-          final Collection<XmlProcessingInstruction> instructions = PsiTreeUtil.findChildrenOfType(prolog, XmlProcessingInstruction.class);
-          for (final XmlProcessingInstruction instruction : instructions) {
-            final ASTNode node = instruction.getNode();
-            final ASTNode nameNode = node.findChildByType(XmlTokenType.XML_NAME);
-            if (nameNode != null && nameNode.getText().equals("import")) {
-              instruction.delete();
-            }
-          }
-          prolog.add(newImportList);
-        } else {
-          document.addBefore(newImportList, document.getRootTag());
-        }
-      }
-    };
-  }
-  
-  private static void collectNamesToImport(@Nonnull final Collection<Pair<String, Boolean>> names, XmlFile file) {
-    file.accept(new JavaFxUsedClassesVisitor() {
-      @Override
-      protected void appendClassName(String fqn) {
-        names.add(Pair.create(fqn, false));
-      }
-    });
-  }
+	@Nonnull
+	@Override
+	public Runnable processFile(final PsiFile file)
+	{
+		VirtualFile vFile = file.getVirtualFile();
+		if(vFile instanceof VirtualFileWindow)
+		{
+			vFile = ((VirtualFileWindow) vFile).getDelegate();
+		}
+		final Project project = file.getProject();
+		if(vFile == null || !ProjectRootManager.getInstance(project).getFileIndex().isInSourceContent(vFile))
+		{
+			return EmptyRunnable.INSTANCE;
+		}
+		final List<Pair<String, Boolean>> names = new ArrayList<>();
+		collectNamesToImport(names, (XmlFile) file);
+		Collections.sort(names, new Comparator<Pair<String, Boolean>>()
+		{
+			@Override
+			public int compare(Pair<String, Boolean> o1, Pair<String, Boolean> o2)
+			{
+				return StringUtil.compare(o1.first, o2.first, true);
+			}
+		});
+		final JavaCodeStyleSettings settings = JavaCodeStyleSettings.getInstance(file);
+		final List<Pair<String, Boolean>> sortedNames = ImportHelper.sortItemsAccordingToSettings(names, settings);
+		final Map<String, Boolean> onDemand = new HashMap<>();
+		ImportHelper.collectOnDemandImports(sortedNames, settings, onDemand);
+		final Set<String> imported = new HashSet<>();
+		final List<String> imports = new ArrayList<>();
+		for(Pair<String, Boolean> pair : sortedNames)
+		{
+			final String qName = pair.first;
+			final String packageName = StringUtil.getPackageName(qName);
+			if(imported.contains(packageName) || imported.contains(qName))
+			{
+				continue;
+			}
+			if(onDemand.containsKey(packageName))
+			{
+				imported.add(packageName);
+				imports.add("<?import " + packageName + ".*?>");
+			}
+			else
+			{
+				imported.add(qName);
+				imports.add("<?import " + qName + "?>");
+			}
+		}
+		final PsiFileFactory factory = PsiFileFactory.getInstance(file.getProject());
 
-  public static abstract class JavaFxUsedClassesVisitor extends XmlRecursiveElementVisitor {
-    @Override
-    public void visitXmlProlog(XmlProlog prolog) {}
+		final XmlFile dummyFile = (XmlFile) factory.createFileFromText("_Dummy_.fxml", XmlFileType.INSTANCE, StringUtil.join(imports, "\n"));
+		final XmlDocument document = dummyFile.getDocument();
+		final XmlProlog newImportList = document.getProlog();
+		if(newImportList == null)
+		{
+			return EmptyRunnable.getInstance();
+		}
+		return new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				final XmlDocument xmlDocument = ((XmlFile) file).getDocument();
+				final XmlProlog prolog = xmlDocument.getProlog();
+				if(prolog != null)
+				{
+					final Collection<XmlProcessingInstruction> instructions = PsiTreeUtil.findChildrenOfType(prolog, XmlProcessingInstruction.class);
+					for(final XmlProcessingInstruction instruction : instructions)
+					{
+						final ASTNode node = instruction.getNode();
+						final ASTNode nameNode = node.findChildByType(XmlTokenType.XML_NAME);
+						if(nameNode != null && nameNode.getText().equals("import"))
+						{
+							instruction.delete();
+						}
+					}
+					prolog.add(newImportList);
+				}
+				else
+				{
+					document.addBefore(newImportList, document.getRootTag());
+				}
+			}
+		};
+	}
 
-    @Override
-    public void visitXmlProcessingInstruction(XmlProcessingInstruction processingInstruction) {}
+	private static void collectNamesToImport(@Nonnull final Collection<Pair<String, Boolean>> names, XmlFile file)
+	{
+		file.accept(new JavaFxUsedClassesVisitor()
+		{
+			@Override
+			protected void appendClassName(String fqn)
+			{
+				names.add(Pair.create(fqn, false));
+			}
+		});
+	}
 
-    @Override
-    public void visitXmlAttribute(XmlAttribute attribute) {
-      final XmlAttributeDescriptor descriptor = attribute.getDescriptor();
-      if (descriptor instanceof JavaFxStaticPropertyAttributeDescriptor) {
-        final PsiElement declaration = descriptor.getDeclaration();
-        if (declaration instanceof PsiMember) {
-          appendClassName((PsiElement)((PsiMember)declaration).getContainingClass());
-        }
-      }
-    }
+	public static abstract class JavaFxUsedClassesVisitor extends XmlRecursiveElementVisitor
+	{
+		@Override
+		public void visitXmlProlog(XmlProlog prolog)
+		{
+		}
 
-    @Override
-    public void visitXmlTag(XmlTag tag) {
-      super.visitXmlTag(tag);
-      final XmlElementDescriptor descriptor = tag.getDescriptor();
-      if (descriptor instanceof JavaFxClassBackedElementDescriptor) {
-        appendClassName(descriptor.getDeclaration());
-      } else if (descriptor instanceof JavaFxPropertyElementDescriptor && ((JavaFxPropertyElementDescriptor)descriptor).isStatic()) {
-        final PsiElement declaration = descriptor.getDeclaration();
-        if (declaration instanceof PsiMember) {
-          appendClassName((PsiElement)((PsiMember)declaration).getContainingClass());
-        }
-      }
-    }
+		@Override
+		public void visitXmlProcessingInstruction(XmlProcessingInstruction processingInstruction)
+		{
+		}
 
-    private void appendClassName(PsiElement declaration) {
-      if (declaration instanceof PsiClass) {
-        appendClassName(((PsiClass)declaration).getQualifiedName());
-      }
-    }
+		@Override
+		public void visitXmlAttribute(XmlAttribute attribute)
+		{
+			final XmlAttributeDescriptor descriptor = attribute.getDescriptor();
+			if(descriptor instanceof JavaFxStaticPropertyAttributeDescriptor)
+			{
+				final PsiElement declaration = descriptor.getDeclaration();
+				if(declaration instanceof PsiMember)
+				{
+					appendClassName((PsiElement) ((PsiMember) declaration).getContainingClass());
+				}
+			}
+		}
 
-    protected abstract void appendClassName(String fqn);
-  }
+		@Override
+		public void visitXmlTag(XmlTag tag)
+		{
+			super.visitXmlTag(tag);
+			final XmlElementDescriptor descriptor = tag.getDescriptor();
+			if(descriptor instanceof JavaFxClassBackedElementDescriptor)
+			{
+				appendClassName(descriptor.getDeclaration());
+			}
+			else if(descriptor instanceof JavaFxPropertyElementDescriptor && ((JavaFxPropertyElementDescriptor) descriptor).isStatic())
+			{
+				final PsiElement declaration = descriptor.getDeclaration();
+				if(declaration instanceof PsiMember)
+				{
+					appendClassName((PsiElement) ((PsiMember) declaration).getContainingClass());
+				}
+			}
+		}
+
+		private void appendClassName(PsiElement declaration)
+		{
+			if(declaration instanceof PsiClass)
+			{
+				appendClassName(((PsiClass) declaration).getQualifiedName());
+			}
+		}
+
+		protected abstract void appendClassName(String fqn);
+	}
 }
