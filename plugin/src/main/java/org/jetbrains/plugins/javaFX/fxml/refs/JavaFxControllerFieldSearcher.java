d8a71a1180f9c1583fd099e4842049e4e136a14f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-13 16:57:41 +0400

    migrate to predicate

--- plugin/src/main/java/org/jetbrains/plugins/javaFX/fxml/refs/JavaFxControllerFieldSearcher.java
+++ plugin/src/main/java/org/jetbrains/plugins/javaFX/fxml/refs/JavaFxControllerFieldSearcher.java
@@ -6,7 +6,6 @@ import com.intellij.java.language.psi.PsiField;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.application.ApplicationManager;
 import consulo.application.ReadAction;
-import consulo.application.util.function.Processor;
 import consulo.content.scope.SearchScope;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.PsiFile;
@@ -19,60 +18,61 @@ import consulo.virtualFileSystem.VirtualFile;
 import consulo.xml.psi.XmlRecursiveElementVisitor;
 import consulo.xml.psi.xml.XmlAttribute;
 import consulo.xml.psi.xml.XmlAttributeValue;
+import jakarta.annotation.Nonnull;
 import org.jetbrains.plugins.javaFX.fxml.FxmlConstants;
 import org.jetbrains.plugins.javaFX.indexing.JavaFxControllerClassIndex;
 
-import jakarta.annotation.Nonnull;
 import java.util.List;
+import java.util.function.Predicate;
 
 @ExtensionImpl
 public class JavaFxControllerFieldSearcher implements ReferencesSearchQueryExecutor {
-  @Override
-  public boolean execute(@Nonnull final ReferencesSearch.SearchParameters queryParameters,
-                         @Nonnull final Processor<? super PsiReference> consumer) {
-    final PsiElement elementToSearch = queryParameters.getElementToSearch();
-    if (elementToSearch instanceof PsiField) {
-      final PsiField field = (PsiField)elementToSearch;
-      final PsiClass containingClass = ReadAction.compute(() -> field.getContainingClass());
-      if (containingClass != null) {
-        final String qualifiedName = ReadAction.compute(() -> containingClass.getQualifiedName());
-        if (qualifiedName != null) {
-          Project project = PsiUtilCore.getProjectInReadAction(containingClass);
-          final List<PsiFile> fxmlWithController =
-            JavaFxControllerClassIndex.findFxmlWithController(project, qualifiedName);
-          for (final PsiFile file : fxmlWithController) {
-            ApplicationManager.getApplication().runReadAction(() -> {
-              final String fieldName = field.getName();
-              if (fieldName == null) {
-                return;
-              }
-              final VirtualFile virtualFile = file.getViewProvider().getVirtualFile();
-              final SearchScope searchScope = queryParameters.getEffectiveSearchScope();
-              if (searchScope.contains(virtualFile)) {
-                file.accept(new XmlRecursiveElementVisitor() {
-                  @Override
-                  public void visitXmlAttributeValue(final XmlAttributeValue value) {
-                    final PsiReference reference = value.getReference();
-                    if (reference != null) {
-                      final PsiElement resolve = reference.resolve();
-                      if (resolve instanceof XmlAttributeValue) {
-                        final PsiElement parent = resolve.getParent();
-                        if (parent instanceof XmlAttribute) {
-                          final XmlAttribute attribute = (XmlAttribute)parent;
-                          if (FxmlConstants.FX_ID.equals(attribute.getName()) && fieldName.equals(attribute.getValue())) {
-                            consumer.process(reference);
-                          }
-                        }
-                      }
+    @Override
+    public boolean execute(@Nonnull ReferencesSearch.SearchParameters queryParameters,
+                           @Nonnull Predicate<? super PsiReference> consumer) {
+        final PsiElement elementToSearch = queryParameters.getElementToSearch();
+        if (elementToSearch instanceof PsiField) {
+            final PsiField field = (PsiField) elementToSearch;
+            final PsiClass containingClass = ReadAction.compute(field::getContainingClass);
+            if (containingClass != null) {
+                final String qualifiedName = ReadAction.compute(containingClass::getQualifiedName);
+                if (qualifiedName != null) {
+                    Project project = PsiUtilCore.getProjectInReadAction(containingClass);
+                    final List<PsiFile> fxmlWithController =
+                        JavaFxControllerClassIndex.findFxmlWithController(project, qualifiedName);
+                    for (final PsiFile file : fxmlWithController) {
+                        ApplicationManager.getApplication().runReadAction(() -> {
+                            final String fieldName = field.getName();
+                            if (fieldName == null) {
+                                return;
+                            }
+                            final VirtualFile virtualFile = file.getViewProvider().getVirtualFile();
+                            final SearchScope searchScope = queryParameters.getEffectiveSearchScope();
+                            if (searchScope.contains(virtualFile)) {
+                                file.accept(new XmlRecursiveElementVisitor() {
+                                    @Override
+                                    public void visitXmlAttributeValue(final XmlAttributeValue value) {
+                                        final PsiReference reference = value.getReference();
+                                        if (reference != null) {
+                                            final PsiElement resolve = reference.resolve();
+                                            if (resolve instanceof XmlAttributeValue) {
+                                                final PsiElement parent = resolve.getParent();
+                                                if (parent instanceof XmlAttribute) {
+                                                    final XmlAttribute attribute = (XmlAttribute) parent;
+                                                    if (FxmlConstants.FX_ID.equals(attribute.getName()) && fieldName.equals(attribute.getValue())) {
+                                                        consumer.test(reference);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        });
                     }
-                  }
-                });
-              }
-            });
-          }
+                }
+            }
         }
-      }
+        return true;
     }
-    return true;
-  }
 }
